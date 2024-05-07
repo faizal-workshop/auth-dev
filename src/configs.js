0899f5bcc57ef45467bbc5e721ffc5be2abadcf4
Faizal Chan <moefc32@gmail.com>
2024-05-07 21:52:41 +0700

    reroute .env

--- src/configs.js
+++ src/configs.js
@@ -1,4 +1,5 @@
-require('dotenv').config();
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '..', '.env') });
 
 module.exports = {
     APP_NAME: process.env.APP_NAME,
