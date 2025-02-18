68e71daf6edcff59ddb683557cf5094725c3caac
Faizal Chan <moefc32@gmail.com>
2025-02-18 22:22:17 +0700

    always give second argument to parseInt()

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -8,7 +8,7 @@ module.exports = {
             script: 'source/app.js',
             env: {
                 HOST: process.env.IPBIND || '127.0.0.1',
-                PORT: parseInt(process.env.PORT) || 4000,
+                PORT: parseInt(process.env.PORT, 10) || 4000,
                 BODY_SIZE_LIMIT: process.env.SIZE_LIMIT || '10M',
             },
         },
