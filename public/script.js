e1494c3003e6a12d27f0f61d7e0a2df957b286f4
Faizal Chan. <moefc32@gmail.com>
2025-02-25 18:29:03 +0700

    remove unused date variable

--- public/script.js
+++ public/script.js
@@ -2,7 +2,6 @@
 
 function alpineData() {
     return {
-        date: new Date().getFullYear(),
         background: {
             get: ['text-bg-primary', 'bg-primary-subtle'],
             post: ['text-bg-success', 'bg-success-subtle'],
@@ -10,7 +9,6 @@ function alpineData() {
             delete: ['text-bg-danger', 'bg-danger-subtle'],
         },
         alpineInit: function () {
-            if (new Date().getFullYear() > 2018) this.date = '2018 - ' + this.date;
             hljs.highlightAll();
         },
     }
